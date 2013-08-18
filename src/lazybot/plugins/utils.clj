9937a7949fe6280fb804bf8b0a7841a2575e5625
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-08-18 02:13:49 -0500

    add hug cmd

--- src/lazybot/plugins/utils.clj
+++ src/lazybot/plugins/utils.clj
@@ -16,6 +16,12 @@
 (defplugin
   
   (:cmd
+    "Hugs"
+    #{"hug"}
+    (fn [{:keys [args] :as com-m}]
+      (send-message com-m (str "hugs " (first args)) :action? true)))
+
+  (:cmd
     "Stares"
     #{"stare"}
     (fn [{:keys [nick bot args] :as com-m}]
