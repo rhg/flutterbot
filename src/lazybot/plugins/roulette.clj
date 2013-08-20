5c283245b32e2ab4c28fb132b2d5bd278a5935a3
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-08-19 23:08:45 -0500

    Fix roulette and add loop

--- src/lazybot/plugins/roulette.clj
+++ src/lazybot/plugins/roulette.clj
@@ -1,15 +1,41 @@
 (ns lazybot.plugins.roulette
-  (:use [lazybot.registry]))
+  (:use [lazybot.registry]
+        [irclj.core :refer [kick]]))
 
-(defn make-pistol [size n]
+(def dead (atom #{}))
+
+(defn make-pistol [size n k]
   "Returns a random seq of false and true of len size with n true"
-  (shuffle (concat (take n (repeat true)) (take (- size n) (repeat false)))))
+  (shuffle (concat (take k (repeat true)) (take (- size n k) (repeat false)) (take n (repeat nil)))))
 
 (def pistol (atom []))
 
-(defn load-pistol [& args]
+(defn load-pistol [com-m msg & args]
   "Assigns (make-pistol ...) to pistol atom"
-  (reset! pistol (apply make-pistol args)))
+  (reset! pistol (apply make-pistol args))
+  (send-message com-m (str msg "."))
+  (reset! dead #{}))
+
+(defn pull [{:keys [nick com channel] :as com-m}]
+  "Pulls the trigger"
+  ;Bullet check
+  (when (seq @pistol)
+    ;Check if dead
+    (if (@dead nick)
+      ;They are
+      (send-message com-m (str "You are dead, " nick))
+      ;They are alive
+      (do
+        (case (peek @pistol)
+          nil (do
+                (send-message com-m (str "drags away " nick "'s body"))
+                (swap! dead conj nick))
+          false (send-message com-m (str nick " gets to live another day"))
+          true (do
+                 (send-message com-m (str "drags away " nick "'s body"))
+                 (kick com channel nick :reason "BANG!")
+                 (swap! dead conj nick)))
+        (swap! pistol pop)))))
 
 (defplugin
 
@@ -17,24 +43,28 @@
     "Load pistol"
     #{"load-pistol" "rload"}
     (fn [{:keys [args] :as com-m}]
-      (if-let [[size n] args]
-        (load-pistol (Integer/parseInt size) (Integer/parseInt n))
-        (load-pistol 6 1))
-      (send-message com-m "The pistol has been loaded.")))
+      (if-let [[size n k] args]
+        (load-pistol com-m "The pistol is loaded" (Integer/parseInt size) (Integer/parseInt n) (Integer/parseInt k))
+        (load-pistol com-m "The pistol is loaded" 6 1 0))))
 
   (:cmd
-    "Loads pistol for lethality"
-    #{"load-lethal" "load-die"}
-    (fn [com-m]
-      (load-pistol 8 6)
-      (send-message com-m "The pistol has been loaded, prepare to die.")))
+    "Loads with kick bullets"
+    #{"load-lethal"}
+    (fn [{:keys [com] :as com-m}]
+      (load-pistol com-m "My name is flutterbot, you have loaded me, now prepare to die" 6 0 5)))
+
+  (:cmd
+    "Loops pull"
+    #{"loop-pull"}
+    (fn [{:keys [nick] :as com-m}]
+      (when-not (@dead nick)
+        (loop [com-m com-m c 0]
+          (pull com-m)
+          (if-not (@dead nick)
+            (recur com-m (inc c))
+            (send-message com-m (str "dead in " (inc c) " pulls")))))))
 
   (:cmd
     "Pulls the trigger"
     #{"pull"}
-    (fn [{:keys [nick] :as com-m}]
-      (when (seq @pistol)
-        (if (peek @pistol)
-          (send-message com-m (str "drags away " nick "'s body") :action? true)
-          (send-message com-m (str nick " gets to live for now")))
-        (swap! pistol pop)))))
+    #'pull))
